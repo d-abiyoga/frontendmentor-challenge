a4524af79a7e89afe2f466ca5439c4619a8fb35a
d-abiyoga <dimas.abiyoga169@gmail.com>
2021-10-18 20:39:41 +0700

    init fylo-dark-theme-landing-page challenge

---
+++ fylo-dark-theme-landing-page-react/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <a href="#">Features</a>
+            <a href="#">Team</a>
+            <a href="#">Sign In</a>
+        </nav>
+    )
+}
+
+export default Nav
